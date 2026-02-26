0dddd973a5f4e8ea3d9d233fa639bbcdb79af6e0
alexmartinezu1018-glitch <alex.martinezu1018@gmail.com>
2026-02-26 16:36:31 -0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -4,3 +4,16 @@ random.shuffle(car_list)
 rounds = 15
 right = 0
 right = 0
+
+print("super car game")
+for number in range(rounds):
+    print("round:", number + 1)
+    print("write this car brand:", car_list[number])
+    user = input("type here:")
+    if user == car_list[number]:
+        print("correct")
+        right += 1
+    else:
+        print("incorrect")
+        wrong += 1
+        
