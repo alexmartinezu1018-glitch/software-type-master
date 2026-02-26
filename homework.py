19e99631176d96ab218919cf393fa5e09d4c7b2d
alexmartinezu1018-glitch <alex.martinezu1018@gmail.com>
2026-02-26 16:11:56 -0300

    Create homework.py

---
+++ homework.py
@@ -0,0 +1,2 @@
+import random
+car_list = ["ferrari", "bugatti", "lamborghini", "mclaren", "pagani", "porsche", "koenigsegg", "fiat", "toyota", "chevrolet", "lexus", "lotus", "maserati", "nissan", "astonmartin"]
