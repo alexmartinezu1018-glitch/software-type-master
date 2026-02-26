e295bf928b0314b8dcb49f32b4466f843b34c036
alexmartinezu1018-glitch <alex.martinezu1018@gmail.com>
2026-02-26 16:31:51 -0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,2 +1,6 @@
 import random
 car_list = ["ferrari", "bugatti", "lamborghini", "mclaren", "pagani", "porsche", "koenigsegg", "fiat", "toyota", "chevrolet", "lexus", "lotus", "maserati", "nissan", "astonmartin"]
+random.shuffle(car_list)
+rounds = 15
+right = 0
+right = 0
